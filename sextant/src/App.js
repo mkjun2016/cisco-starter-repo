e5c1b3b2295ba78a86bf1299a238cebf0eec1de9
Kevin <53570728+mkjun2016@users.noreply.github.com>
2023-09-11 23:37:29 +0900

    changes

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import { Component } from 'react';
 
+const Banner = ({ children }) => {
+  return (
+    <div style={{
+      backgroundColor: '#333',
+      color: '#eee',
+      minHeight: 50,
+      padding: 20,
+      flex: 1,
+      fontSize: 20,
+      fontWeight: "bold",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "row",
+      gap: 20
+    }}>
+      <p>{children}</p>
+      <p>Network</p>
+      <p>Contacts</p>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="banner">
+        <Banner>
+          Sextant Website
+        </Banner>
       </div>
     );
   }
